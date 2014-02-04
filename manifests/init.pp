7c0466a0e4bccf9e8a1e4922d21eccf8285f0cce
Mariusz Gliwiński <mariusz@ifresearch.org>
2014-02-04 12:15:37 +0000

    new thrift module implementation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,101 +1,81 @@
-class thrift {
-  $yum_pkgs = [ "boost-devel",
-                "boost-test",
-                "boost-program-options",
-                "libevent-devel",
-                "automake",
-                "libtool",
-                "flex",
-                "bison",
-                "pkgconfig",
-                "gcc-c++",
-                "openssl-devel",]
-  $apt_pkgs = [ "libboost-dev",
-                "libboost-test-dev",
-                "libboost-program-options-dev",
-                "libevent-dev",
-                "automake",
-                "libtool",
-                "flex",
-                "bison",
-                "pkg-config",
-                "g++",
-                "libssl-dev",]
+class thrift( $version="0.9.1", $executable="/usr/local/bin/thrift" )
+{
+	$yum_pkgs = [ "boost-devel",
+		"boost-test",
+		"boost-program-options",
+		"libevent-devel",
+		"automake",
+		"libtool",
+		"flex",
+		"bison",
+		"pkgconfig",
+		"gcc-c++",
+		"openssl-devel",
+	]
+	$apt_pkgs = [ "libboost-dev",
+		"libboost-test-dev",
+		"libboost-program-options-dev",
+		"libevent-dev",
+		"automake",
+		"libtool",
+		"flex",
+		"bison",
+		"pkg-config",
+		"g++",
+		"libssl-dev",
+	]
+	$digest_type = "md5"
+	$thrift_url = "http://archive.apache.org/dist/thrift/${version}/thrift-${version}.tar.gz"
+	$digest_url = "http://archive.apache.org/dist/thrift/${version}/thrift-${version}.tar.gz.${digest_type}"
+	$archive_dir = "/usr/src"
+	$thrift_dir = "/usr/local/lib"
 
-  if $::osfamily == "RedHat" {
-    $pkgs = $yum_pkgs
-  }
-  elsif $::operatingsystem == "Amazon" {
-    $pkgs = $yum_pkgs
-  }
-  else {
-    $pkgs = $apt_pkgs
-  }
+	if $::osfamily == "RedHat"
+	{
+		$pkgs = $yum_pkgs
+	}
+	elsif $::operatingsystem == "Amazon"
+	{
+		$pkgs = $yum_pkgs
+	}
+	else
+	{
+		$pkgs = $apt_pkgs
+	}
 
-  package { $pkgs:
-    ensure => present,
-    before => Instool['thrift-0.9.1'],
-  }
+	package { $pkgs :
+		ensure => present,
+	}
 
-  #package { 'rspec':
-  #  ensure   => 'installed',
-  #  provider => 'gem',
-  #  before   => Instool['thrift-0.9.0'],
-  #}
+	# $pkgs2 = [ 'curl', 'unzip', 'tar', ]
+	# package { $pkgs2 :
+	# ensure => present,
+	# }
 
-  instool { "thrift-0.9.1":
-    url  => "http://archive.apache.org/dist/thrift/0.9.1/thrift-0.9.1.tar.gz",
-    onlyif => [
-      "test ! -x /usr/local/bin/thrift"
-    ]
-  }
+	# include "archive::prerequisites"
 
-}
+	package { ['unzip','tar'] :
+		ensure => present
+	}
 
-define instool (
-  $thing=$title,
-  $dest="/usr/local/lib",
-  $onlyif=undef,
-  $url
-) {
-  $tmpdir = "/tmp/${thing}"
-  $instdir = "${dest}/${thing}"
-  $buildpkgs = ["tar", "make"]
-
-  include wget
-
-  package { $buildpkgs:
-    ensure => present,
-  }
-
-  file {$tmpdir:
-    ensure  => directory,
-  }
-
-  notify {"${tmpdir} is ensured":
-    subscribe => File[$tmpdir],
-  }
-
-  exec{"download_and_untar":
-    provider => shell,
-    command  => "wget -qO- ${url} | tar xzf - -C /tmp",
-    onlyif   => $onlyif;
-  }
-
-  file{$instdir:
-    ensure  => directory,
-    recurse => true,
-    source  => $tmpdir;
-  }
-
-  exec{["./configure --without-cpp --without-python", "make", "make install", "make clean"]:
-    provider => shell,
-    cwd      => $instdir,
-    onlyif   => $onlyif;
-  }
-
-  notify {"install ${name} from ${url} to ${dest}/${name}":}
-
-  File[$tmpdir] -> Package[$buildpkgs] -> Exec['download_and_untar'] -> File[$instdir] ->
-  Exec["./configure --without-cpp --without-python"] -> Exec['make'] -> Exec['make install'] -> Exec['make clean']
+	archive { "thrift-$version" :
+		ensure => present,
+		url => $thrift_url,
+		digest_url => $digest_url,
+		digest_type => $digest_type,
+		allow_insecure => true,
+		src_target => $archive_dir,
+		target => $thrift_dir,
+		dependency_class => Package[ 'unzip', 'tar' ]
+	}
+	->
+	exec{ "./configure --without-cpp --without-python --without-php_extension && make && make install && make clean" :
+		provider => shell,
+		cwd      => "$thrift_dir/thrift-${version}",
+		onlyif   => $onlyif,
+		timeout  => 0,
+		require  => Package[ $pkgs ]
+	}
+	->
+	notify { "Thrift compiler installed at $target" : }
 }
